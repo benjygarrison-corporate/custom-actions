75d85df0e2b429a09c40a33fb3a7c76409217a6f
ben garrison <benjamin.garrison@sap.com>
2024-06-13 12:02:54 -0700

    add urk

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,10 +1,8 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    exec.exec('echo "Exec"');
-    core.notice('Core Notice!');
-    github.notice('GitHub Notice!');
+    const url = 'https://docs.github.com/en/enterprise-cloud@latest/actions/using-workflows/reusing-workflows';
+    core.setOutput('url', url);
 }
 run();
