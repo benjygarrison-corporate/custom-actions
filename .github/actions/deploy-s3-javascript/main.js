231d83d9d603444cbef84f8c468c9979a856a313
ben garrison <benjamin.garrison@sap.com>
2024-06-13 11:43:38 -0700

    add consts

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,10 @@
 const core = require('@actions/core');
-// const github = require('@actions/github');
-// const exec = require('@actions/exec');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 function run() {
-    core.notice('Hello World!');
+    exec.exec('echo "Exec"');
+    core.notice('Core Notice!');
+    github.notice('GitHub Notice!');
 }
 run();
